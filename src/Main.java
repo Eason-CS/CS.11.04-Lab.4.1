8604cc47d2bdba04ca31df982cbe9e4b447b23fd
techarenz <57818506+techarenz@users.noreply.github.com>
2023-08-19 14:15:04 +0800

    Create README.md

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,51 @@
 public class Main {
 
-    // 1. parenthesesCheck
+    public static void main(String[] args) {
+        System.out.println(parenthesesCheck("()")); // Output: true
+        System.out.println(parenthesesCheck(")(()))")); // Output: false
+        System.out.println(parenthesesCheck("(")); // Output: false
+        System.out.println(parenthesesCheck("(())((()())())")); // Output: true
 
+        System.out.println(reverseInteger(1234)); // Output: "4321"
+        System.out.println(reverseInteger(2468)); // Output: "8642"
 
-    // 2. reverseInteger
+        System.out.println(encryptThis("Hello good day")); // Output: "72olle 103doo 100ya"
+        System.out.println(encryptThis("Ready set go")); // Output: "82yade 115te 103o"
+    }
 
+    public static boolean parenthesesCheck(String str) {
+        int count = 0;
+        for (char c : str.toCharArray()) {
+            if (c == '(') {
+                count++;
+            } else if (c == ')') {
+                count--;
+            }
+            if (count < 0) {
+                return false;
+            }
+        }
+        return count == 0;
+    }
 
-    // 3. encryptThis
+    public static String reverseInteger(int num) {
+        return new StringBuilder(Integer.toString(num)).reverse().toString();
+    }
 
-
-    // 4. decipherThis
-
-
+    public static String encryptThis(String message) {
+        StringBuilder result = new StringBuilder();
+        for (String word : message.split(" ")) {
+            if (result.length() > 0) {
+                result.append(" ");
+            }
+            int wordLength = word.length();
+            result.append((int) word.charAt(0));
+            if (wordLength > 1) {
+                result.append(word.charAt(wordLength - 1));
+                result.append(word, 2, wordLength - 1);
+                result.append(word.charAt(1));
+            }
+        }
+        return result.toString();
+    }
 }
